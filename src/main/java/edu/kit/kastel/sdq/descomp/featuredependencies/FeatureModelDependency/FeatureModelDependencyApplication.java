2d5b2e1418faa4c4bf81bb5814e51e80787c5b8f
Sandro <layornos@gmail.com>
2020-02-05 14:03:50 +0100

    init project

---
+++ src/main/java/edu/kit/kastel/sdq/descomp/featuredependencies/FeatureModelDependency/FeatureModelDependencyApplication.java
@@ -0,0 +1,13 @@
+package edu.kit.kastel.sdq.descomp.featuredependencies.FeatureModelDependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureModelDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureModelDependencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/kit/kastel/sdq/descomp/featuredependencies/FeatureModelDependency/FeatureModelDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.kastel.sdq.descomp.featuredependencies.FeatureModelDependency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureModelDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
